96373df5fec3d1adfc5815ea4a8f27b89d358305
Mathieu Perrot <mathieuperrot@Cefimmac55.local>
2021-12-03 09:24:46 +0100

    My first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var mobileTrigger = document.getElementsByClassName('menu-toggler');
+var mobileMenu = document.getElementsByClassName('menu-header');
+
+$(mobileTrigger).click(function(){
+    $(mobileMenu).toggleClass('opened')
+    $(mobileTrigger).toggleClass('active')
+})
+
